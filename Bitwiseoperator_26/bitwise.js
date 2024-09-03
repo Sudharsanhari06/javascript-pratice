032b5a28b68de948985c4c2ead280bfd070f8e80
Sudharsanharo06 <sudharsanhari2006@gmail.com>
2024-09-03 13:50:25 +0530

    Initial Commit

---
+++ Bitwiseoperator_26/bitwise.js
@@ -0,0 +1,37 @@
+// Write code to find the value of  (A AND B) AND C
+// let a=0;
+// let b=1;
+// let c=5;
+// let and=(a&b)&c;
+// console.log(and);
+
+// Write code to find value of A XOR B\
+
+// let num1=4;
+// let num2=4;
+// let total=num1^num2;
+// console.log(total);
+
+// Write code to shift left by a given variable.
+
+// let num3=6;
+// let leftShift=num3>>2;
+// console.log(leftShift);
+
+
+// let num1=5;
+// let num2=7;
+// let num3=num1^num2;
+// console.log(num3);
+
+// let num4=8;
+// let total=(num3&num4);
+// console.log(total);
+
+// let leftShift=total<<2;
+// console.log(leftShift);
+
+// let toNot=num1;
+// let toOr=leftShift|toNot;
+// console.log(toOr);
+
---
+++ helloworld.js
@@ -0,0 +1,2 @@
+console.log("About me");
+console.log("Hii I am sudharsan. and  I am from tirunelveli. Currently i am studying freshworks software Acadamy.");
