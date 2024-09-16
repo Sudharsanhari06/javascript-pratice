16cf2cc101d0e76a63abb3eed0a49879dc66feca
Sudharsanharo06 <sudharsanhari2006@gmail.com>
2024-09-16 21:50:30 +0530

    array file

---
+++ Array/problem-1.js
@@ -0,0 +1,12 @@
+// Array program
+
+let language=["javascript","python","c++","java"];
+console.log(language);
+
+for(let i=0;i<language.length;i++){
+    console.log(language[i]);
+}
+
+
+
+
---
+++ Array/problem-2.js
@@ -0,0 +1,8 @@
+// reverse the array.
+
+let fruits=["apple","mango","grapes","banana"];
+// console.log(fruits);
+let to=fruits.length-1;
+for(let i=0;to>=i;to--){//4>=0,to=3
+    console.log(fruits[to]);
+}
