0eb02ff265026abb273046765864db1180dc167a
Sudharsanhari06 <sudharsanhari2006@gmail.com>
2024-12-30 10:03:20 +0530

    rename file

---
+++ Pratice/Pratice/script.js
@@ -0,0 +1,32 @@
+let p=10000;
+let r=5;
+let t=3;
+let simpleInterest=(p*r*t)/100;
+console.log(simpleInterest);
+
+
+let radius=14;
+let pi=3.14159;
+let area=pi*radius*radius;
+console.log(area);
+
+let priceA=150;
+let priceB=200;
+priceA= priceA+priceB;
+priceB= priceA-priceB;
+priceA= priceA-priceB;
+console.log(priceA,priceB);
+
+
+let num1=85;
+let num2=90;
+let num3=95;
+let average=(num1+num2+num3)/3;
+console.log(average);
+
+
+let profit=5000;
+let R=6;
+let T=4;
+let compound=profit*(1+(R/100))^T;
+
